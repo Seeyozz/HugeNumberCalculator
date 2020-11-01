b3c7bcefa124a7b05f57314eb574747e192435fb
Benjamin <benjamin.kalfa@efrei.net>
2020-11-01 15:39:28 +0100

    Add main.c file

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "IHMHugeNumber/ScanHugeNumber.h"
-#include "IHMHugeNumber/ShowHugeNumber.h"
+//#include "IHMHugeNumber/ScanHugeNumber.h"
+//#include "IHMHugeNumber/ShowHugeNumber.h"
 
 int main (void) {
-    HugeFloat* op1 = createHugeFloatFromString ("1002315684321510", "-10");
+    /*HugeFloat* op1 = createHugeFloatFromString ("1002315684321510", "-10");
     HugeFloat* op2 = createHugeFloatFromString ("98745", "236");
     HugeFloat* addition = addHugeFloat (op1, op2);
     HugeFloat* substraction = substractHugeFloat (op1, op2);
     HugeFloat* multiplication = multiplyHugeFloat (op1, op2);
-    HugeInt* division = divideHugeInt (op1->significand, op2->significand);
+    HugeInt* division = divideHugeInt (op1->significand, op2->significand);*/
 
-    printHugeFloat (op1);
-    printHugeFloat (op2);
+    //printHugeFloat (op1);
+    //printHugeFloat (op2);
     printf (" + = ");
-    printHugeFloat (addition);
+    //printHugeFloat (addition);
     printf (" - = ");
-    printHugeFloat (substraction);
+    //printHugeFloat (substraction);
     printf (" x = ");
-    printHugeFloat (multiplication);
+    //printHugeFloat (multiplication);
     printf (" / = ");
-    printHugeInt (division);
+    //printHugeInt (division);
 
-    deleteHugeFloat (op1);
+    /*deleteHugeFloat (op1);
     deleteHugeFloat (op2);
     deleteHugeFloat (addition);
     deleteHugeFloat (substraction);
     deleteHugeFloat (multiplication);
-    deleteHugeInt (division);
+    deleteHugeInt (division);*/
 
     return EXIT_SUCCESS;
 }
